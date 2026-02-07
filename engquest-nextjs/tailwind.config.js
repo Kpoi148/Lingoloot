d4b1e80faa17406fe9d3c5fee205ae41b0e9f8da
Khanh_Poi <157507406+KhanhDesignWeb@users.noreply.github.com>
2026-02-07 23:44:12 +0700

    revert: remove dynamic background

--- engquest-nextjs/tailwind.config.js
+++ engquest-nextjs/tailwind.config.js
@@ -32,9 +32,19 @@ module.exports = {
           "0%": { transform: "translateX(-150%) skewX(-25deg)" },
           "100%": { transform: "translateX(150%) skewX(-25deg)" },
         },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
         shimmer: "shimmer 2.5s infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
